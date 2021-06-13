04215d23d70c0ebf304651a72aa3eb0f800d40c1
czq <zschenzq@outlook.com>
2021-06-13 21:38:12 +0800

    update dev

--- Typescript/类/src/index.ts
+++ Typescript/类/src/index.ts
@@ -1,3 +1,14 @@
-// Class 
-//we test rebase
-// we test rebase2
+// about Class
+// 普通的类的注解
+class Greeter {
+  greeting: string; // 创建greeting属性
+  // 构造函数
+  constructor(message: string) {
+    this.greeting = message; // 令当前的greeting属性 = message参数
+  }
+  greet() {
+    // 方法 greet
+    return 'Hello, ' + this.greeting;
+  }
+}
+let greeter = new Greeter('world');
